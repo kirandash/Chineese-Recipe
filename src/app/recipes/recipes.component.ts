10a8bf424e6eb2fcb1d5a64e23b14ef5a9829e0a
kirandash <thekirankumardash@gmail.com>
2017-05-14 11:40:04 +0530

    Create recipes component

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cr-recipes',
+  templateUrl: './recipes.component.html'
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
